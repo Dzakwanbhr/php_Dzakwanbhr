eb5afb8dc1e7b48be6744cd28976dc73164d26b0
Dzakwan B.P. <133729274+Dzakwanbhr@users.noreply.github.com>
2025-09-03 19:33:23 +0700

    soal1.php

--- soal1.php
+++ soal1.php
@@ -1,16 +1,26 @@
 <?php
-
 $jml = $_GET['jml'];
-echo "<table border=1>\n";
-for ($a = $jml; $a > 0; $a--)
-{
-  echo "<tr>\n";
-  for ($b = $a; $b > 0; $b--)
-  {
-    echo "<td>$b</td>";
-  }
-  echo "</tr>\n";
-}
-echo "</table>";
+echo "<table border=1 cellspacing=0 cellpadding=5>\n";
 
+for ($a = $jml; $a > 0; $a--) {
+    // total tiap baris
+    $total = 0;
+    for ($b = $a; $b > 0; $b--) {
+        $total += $b;
+    }
+
+    // baris total
+    echo "<tr>";
+    echo "<td colspan='$a' align='center'><b>TOTAL = $total</b></td>";
+    echo "</tr>\n";
+
+    // baris angka
+    echo "<tr>";
+    for ($b = $a; $b > 0; $b--) {
+        echo "<td align='center'>$b</td>";
+    }
+    echo "</tr>\n";
+}
+
+echo "</table>";
 ?>
